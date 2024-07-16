9be0007e660d5bef22415d8f478c0cc75f2e0f3d
Dzunisani <dngobeni023@student.wethinkcode.co.za>
2024-07-16 12:39:27 +0200

    Modified index.html, programming.js and styles.css files

--- programming.js
+++ programming.js
@@ -1,5 +1,11 @@
-// const button = document.getElementsByTagName("button")[0];
-// console.log(button)
-// button.addEventListener("click", () => {
-//     button.classList.add("clicked")
-// })
+const button = document.getElementsByTagName("button")[0];
+console.log(button)
+const aside = document.getElementsByClassName("left-right-sections")[0];
+console.log(aside)
+button.addEventListener("click", () => {
+    // button.classList.add("clicked")
+    console.log("button has been clicked!!!!")
+    button.classList.toggle("show")
+    button.style.backgroundColor = "red";
+    aside.style.display = "block";
+})
